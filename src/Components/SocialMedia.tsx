d8e6a2a1cfdabf50c0eaf718cf325e705a41b753
Michael Douglas <michaeldouglas2004@hotmail.com>
2025-03-23 11:40:25 -0300

    feat: add SocialMedia component

---
+++ src/Components/SocialMedia.tsx
@@ -0,0 +1,18 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+export function SocialMedia(){
+    return(
+        <View className="mt-2">
+            <Text className="font-semibold text-xl text-center text-[#70bac2]">Social media login</Text>
+
+            <View className="px-16 flex-row justify-around mt-7">
+                <TouchableOpacity>
+                    <Image source={require("../assets/google.png")} className="w-12 h-12"/>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image source={require("../assets/apple.png")} className="w-12 h-12"/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
