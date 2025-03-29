ad7ed8a03b57df8923cdb30456b730ace97190a8
Michael Douglas <michaeldouglas2004@hotmail.com>
2025-03-29 19:22:37 -0300

    fix: change icons Socialmedia component

--- src/Components/SocialMedia.tsx
+++ src/Components/SocialMedia.tsx
@@ -2,15 +2,15 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export function SocialMedia(){
     return(
-        <View className="mt-2">
-            <Text className="font-semibold text-xl text-center text-[#70bac2]">Fazer login com:</Text>
+        <View className="my-2">
+            <Text className="font-semibold text-xl text-center text-[#70bac2]">Participe das nossas comunidades:</Text>
 
             <View className="px-16 flex-row justify-around mt-7">
                 <TouchableOpacity>
-                    <Image source={require("../assets/google.png")} className="w-12 h-12"/>
+                    <Image source={require("../assets/whatsapp.png")} className="w-12 h-12"/>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Image source={require("../assets/apple.png")} className="w-12 h-12"/>
+                    <Image source={require("../assets/instagram.png")} className="w-12 h-12"/>
                 </TouchableOpacity>
             </View>
         </View>
