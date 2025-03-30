e896e020e27dfc5e5acc658a97a3a15e80431df9
Michael Douglas <michaeldouglas2004@hotmail.com>
2025-03-29 22:19:13 -0300

    feat add Screen Profile

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import { Article } from "./src/app/Article";
 
 import { Ebook } from "./src/app/E-book";
 import { Home } from "./src/app/Home";
+import { Profile } from "./src/app/Proflie";
 
 export default function App() {
   return (
     <>
-      <Article/>
+      <Profile/>
       {/* <Navigation/> */}
     </>
   );
---
+++ src/app/Proflie.tsx
@@ -0,0 +1,37 @@
+import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Avatar, AvatarImage } from "../Components/Avatar";
+
+export function Profile() {
+  return (
+    <View className="flex-1 bg-[#F5EBE0]">
+      <View className="items-center py-8">
+        <Image source={require("../assets/Logo 1.png")} className="w-56 h-56"/>
+      </View>
+
+      <View className="items-center">
+        <Avatar className="h-56 w-56">
+            <AvatarImage
+            source={{
+                uri: "https://pbs.twimg.com/profile_images/1745949238519803904/ZHwM5B07_400x400.jpg",
+            }}
+            />
+        </Avatar>
+      </View>
+      <TouchableOpacity className="items-end relative right-24 bottom-8">
+        <Image source={require("../assets/camera.png")} className="w-10 h-10"/>
+      </TouchableOpacity>
+
+      <View className="px-6 h-60 justify-evenly">
+        <View className="gap-3">
+          <Text className="text-[#ed967d] text-lg">Nome Completo</Text>
+          <Text className="left-3 text-lg">Nome Completo</Text>
+          <Text className="text-[#ed967d] text-lg">Tel(DDD)</Text>
+          <Text className="left-3 text-lg">Tel(DDD)</Text>
+          <Text className="text-[#ed967d] text-lg">Email</Text>
+          <Text className="left-3 text-lg">Email</Text>
+        </View>
+        </View>
+
+    </View>
+  );
+}
